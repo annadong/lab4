c5968c7a1a145c63b085eea7147b03674040b4da
Anna Dong <annadong@v1020-wn-34-234.campus-dynamic.uwaterloo.ca>
2018-12-06 14:54:48 -0500

    added worst fit implementaiton

--- lab4/starter/mem.c
+++ lab4/starter/mem.c
@@ -2,8 +2,8 @@
  * @file memory.c
  * @brief: ECE254 Lab: memory allocation algorithm comparison template file
  * @author: 
- * @date: 2015-11-20
  */
+ * @date: 2015-11-20
 
 /* includes */
 #include <stdio.h> 
@@ -21,7 +21,7 @@ typedef struct node {
 
 /* global variables */
 node_t* bfHead;
-
+node_t* wfHead;
 /* Functions */
 
 /* memory initializer */
@@ -40,10 +40,7 @@ int best_fit_memory_init(size_t size)
 	bfHead->isFree = 1;
 	bfHead->next = NULL;
 	bfHead->prev = NULL;
-	
-	printf("initialize size: %lu\n",(unsigned long) bfHead->size);
-	printf("size of node_t: %lu\n", (unsigned long) sizeof(node_t));
-	printf("size inputted: %lu\n", (unsigned long) size);	
+
 	return 0;
 
 }
@@ -53,6 +50,17 @@ int worst_fit_memory_init(size_t size)
 
 	// To be completed by students
 	// You call malloc once here to obtain the memory to be managed.
+
+	if (size < sizeof(node_t) + 4) {
+		return -1;
+	}
+
+	wfHead = malloc(size);
+	wfHead->size = size - sizeof(node_t);
+	wfHead->isFree = 1;
+	wfHead->next = NULL;
+	wfHead->prev = NULL;
+
 	return 0;
 
 }
@@ -115,7 +123,55 @@ void *best_fit_alloc(size_t size)
 void *worst_fit_alloc(size_t size)
 {
 	// To be completed by students
-	return NULL;
+
+	size_t thisSize = size;
+	node_t *cur = wfHead;
+	
+	size_t maxSize = 0;
+	node_t* biggestNode = NULL;
+
+	thisSize += (4-thisSize%4)%4;
+
+	while (cur!= NULL) {
+		if (cur->size >= thisSize && cur->size > maxSize && cur->isFree == 1) {
+			maxSize = cur->size;
+			biggestNode = cur;
+		}
+		cur = cur->next;
+	}
+
+	if (biggestNode == NULL) {
+		return NULL;
+	}
+	printf("size of biggestNode node: %lu\n", (unsigned long) biggestNode->size);
+
+	if (thisSize == biggestNode->size) {
+		smallestNode->isFree = 0;
+	} else {
+		size_t remainingSize = biggestNode->size - thisSize;
+
+		if (remainingSize < sizeof(node_t) + 4) {
+			biggestNode->isFree = 0;
+		}else {
+			node_t* newNode = (node_t *)((size_t) biggestNode + thisSize +sizeof(node_t));
+			newNode->size = biggestNode->size - thisSize - sizeof(node_t);
+			newNode->prev = biggestNode;
+			newNode->next = biggestNode->next;
+	
+			node_t* next = biggestNode->next;
+		
+			biggestNode->next = newNode;
+			if(next != NULL) {
+				next->prev = newNode;
+			}
+			newNode->isFree = 1;
+			 biggestNode->isFree = 0;
+			
+			biggestNode->size = thisSize;
+		}
+	}
+
+	return (node_t *)((size_t)biggestNode+sizeof(node_t));
 }
 
 /* memory de-allocator */
@@ -184,6 +240,59 @@ void worst_fit_dealloc(void *ptr)
 {
 
 	// To be completed by students
+	if (!ptr) {
+		return;
+	}
+
+	node_t* addressToFind = (node_t*) ((size_t)(ptr)- sizeof(node_t));
+
+	node_t* nodeToDealloc = NULL;
+	node_t* curNode = wfHead;
+
+	while (curNode != NULL) {
+		if (curNode == addressToFind) {
+			nodeToDealloc = curNode;
+			break;
+		}
+		curNode = curNode->next;
+	}
+
+	if (!nodeToDealloc) {
+		return;
+	}
+
+	nodeToDealloc->isFree = 1;
+
+	size_t newSize = nodeToDealloc->size;
+	
+	//if the next node is free, merge
+	if(nodeToDealloc-> next != NULL) {
+		if (nodeToDealloc->next->isFree == 1) {
+			newSize += nodeToDealloc->next->size + sizeof(node_t);
+			node_t* newNext = nodeToDealloc->next->next;
+			nodeToDealloc->next = newNext;
+			if(newNext != NULL) {
+				newNext->prev = nodeToDealloc;
+			}
+			nodeToDealloc->size = newSize;
+		}
+	}
+
+	//if the previous node is free, merge
+	if(nodeToDealloc->prev != NULL) {
+		if (nodeToDealloc->prev->isFree == 1) {
+			newSize += nodeToDealloc->prev->size + sizeof(node_t);
+			node_t* prev = nodeToDealloc->prev;
+			node_t* next = nodeToDealloc->next;
+			prev->next = next;
+			if(next != NULL) {
+				next->prev = prev;
+			}
+			prev->size = newSize;
+			prev->isFree = 1;
+		}
+	}
+
 	return;
 }
 
@@ -208,6 +317,16 @@ int best_fit_count_extfrag(size_t size)
 int worst_fit_count_extfrag(size_t size)
 {
 	// To be completed by students
+	node_t* cur = wfHead;
+	int count = 0;
+	while (cur != NULL) {
+		printf("cur size: %lu\n", (unsigned long) cur->size);
+		if (cur->isFree == 1 && cur->size < size ) {
+			count += 1;
+		}
+		cur = cur->next;
+	}
+
 	return 0;
 }
 
